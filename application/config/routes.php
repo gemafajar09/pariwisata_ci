d511e68ae49e80d7e2f686a5a36aa1842e549887
gemafajar09 <gemafajarramadhan09@gmail.com>
2022-07-11 11:11:03 +0700

    fe1

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$route['default_controller'] = 'B_login';
+$route['default_controller'] = 'F_home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 
